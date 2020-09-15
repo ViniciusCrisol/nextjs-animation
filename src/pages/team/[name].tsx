cc517e8cd31ae3768fbb5327b25e142b52fca580
ViníciusCrisol <vinicius.crisol@hotmail.com>
2020-09-15 17:32:22 -0300

    Get Props by path

--- src/pages/team/[name].tsx
+++ src/pages/team/[name].tsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import { NextPage } from 'next';
 
+import api from '../../services/api';
+import { useFetch } from '../../hooks/useFetch';
+
 interface HomeProps {
   initialData: TeamProps;
 }
 
-const Team: NextPage<HomeProps> = () => {
-  return <div />;
+const Team: NextPage<HomeProps> = ({ initialData }) => {
+  const { data: team } = useFetch<TeamProps>('/', initialData);
+
+  return (
+    <div>
+      <h1>{team.name}</h1>
+    </div>
+  );
+};
+
+Team.getInitialProps = async ({ query }) => {
+  console.log();
+
+  const response = await api.get(`/?name=${query.name}`);
+
+  console.log(response[0]);
+
+  return { initialData: response.data };
 };
 
 export default Team;
